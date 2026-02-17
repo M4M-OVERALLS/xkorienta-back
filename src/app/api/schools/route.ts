ff31cf67377be28ff40101bbe5db962b69f4fc93
JuniorMSL <junior@msl-itech.com>
2026-02-17 02:41:04 +0100

    modify school

--- src/app/api/schools/route.ts
+++ src/app/api/schools/route.ts
@@ -1,5 +1,7 @@
+import connectDB from "@/lib/mongodb";
 import { SchoolController } from "@/lib/controllers/SchoolController";
 
 export async function GET(req: Request) {
+    await connectDB();
     return SchoolController.getSchools(req);
 }
